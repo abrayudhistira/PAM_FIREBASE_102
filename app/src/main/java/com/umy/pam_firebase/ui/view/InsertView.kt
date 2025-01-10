5d991644612a19c6fe640adb1b5093e41adb6d9d
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-01-10 19:53:07 +0700

    fungsi insert view

--- app/src/main/java/com/umy/pam_firebase/ui/view/InsertView.kt
+++ app/src/main/java/com/umy/pam_firebase/ui/view/InsertView.kt
@@ -1,4 +1,16 @@
 package com.umy.pam_firebase.ui.view
 
-class InsertView {
-}
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.umy.pam_firebase.ui.viewmodel.InsertViewModel
+import com.umy.pam_firebase.ui.viewmodel.PenyediaViewModel
+
+// fungsi insert view
+@Composable
+fun InsertMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
