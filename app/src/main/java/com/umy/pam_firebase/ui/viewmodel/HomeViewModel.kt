8739aff378967c1fb627015ba983efd1e962e90c
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-01-10 18:56:58 +0700

    Menambahkan viewmodel untuk delete

--- app/src/main/java/com/umy/pam_firebase/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/umy/pam_firebase/ui/viewmodel/HomeViewModel.kt
@@ -37,6 +37,16 @@ class HomeViewModel(
                 }
         }
     }
+
+    fun deleteMhs(mahasiswa: Mahasiswa) { // ViewModel untuk delete
+        viewModelScope.launch {
+            try {
+                mhs.deleteMhs(mahasiswa)
+            } catch (e: Exception) {
+                mhsUiState = HomeUiState.Error(e)
+            }
+        }
+    }
 }
 
 sealed class HomeUiState {
