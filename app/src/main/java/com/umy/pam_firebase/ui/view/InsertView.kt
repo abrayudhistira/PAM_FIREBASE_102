f7d38094c237a5b62b261e1b1f292b63fdfb8f7c
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-01-10 20:01:01 +0700

    Event perubahan state untuk snackbar dan navigasi

--- app/src/main/java/com/umy/pam_firebase/ui/view/InsertView.kt
+++ app/src/main/java/com/umy/pam_firebase/ui/view/InsertView.kt
@@ -1,10 +1,17 @@
 package com.umy.pam_firebase.ui.view
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.umy.pam_firebase.ui.viewmodel.FormState
 import com.umy.pam_firebase.ui.viewmodel.InsertViewModel
 import com.umy.pam_firebase.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 // fungsi insert view
 @Composable
@@ -16,4 +23,31 @@ fun InsertMhsView(
 ) {
     val uiState = viewModel.uiState // State utama untuk loading, success, error
     val uiEvent = viewModel.uiEvent // State untuk form dan validasi
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    // Event perubahan state untuk snackbar dan navigasi
+    LaunchedEffect(uiState) {
+        when (uiState) {
+            is FormState.Success -> {
+                println(
+                    "InsertMhsView: uiState is FormState.Success, navigate to home " + uiState.message
+                )
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message) //Tampilkan snackbar
+                }
+                delay(700)
+                // Navigasi langsung
+                onNavigate()
+                viewModel.resetSnackBarMessage() // Reset snackbar state
+            }
+            is FormState.Error -> {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+            }
+            else -> Unit
+        }
+    }
+
 }
