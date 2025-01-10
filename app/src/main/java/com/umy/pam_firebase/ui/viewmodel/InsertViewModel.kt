cd73c9ef28e95defc0da418a1671f897d78e6c74
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-01-10 19:11:39 +0700

    // Memperbarui state by input pengguna

--- app/src/main/java/com/umy/pam_firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/umy/pam_firebase/ui/viewmodel/InsertViewModel.kt
@@ -1,10 +1,23 @@
 package com.umy.pam_firebase.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.umy.pam_firebase.repository.RepositoryMhs
 
 class InsertViewModel (
     private val mhs: RepositoryMhs // deklarasi variabel mhs
 ) : ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
 
+    // Memperbarui state by input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent =uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
 }
