06bdbb363f52a2415ccc47015c78114b6a23d4d5
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-01-10 20:39:44 +0700

    membuat interface DestinasiNavigasi dan object DestinasiHome serta DestinasiInsert

--- app/src/main/java/com/umy/pam_firebase/ui/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/umy/pam_firebase/ui/navigasi/DestinasiNavigasi.kt
@@ -1,2 +1,14 @@
 package com.umy.pam_firebase.ui.navigasi
 
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+object DestinasiInsert : DestinasiNavigasi {
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
